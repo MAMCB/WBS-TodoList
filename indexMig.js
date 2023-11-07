fbfb542533cdd9e0387e33746490d1039c694ab8
MAMCB <96302464+MAMCB@users.noreply.github.com>
2023-11-07 13:48:32 +0100

    refactored add task function

--- indexMig.js
+++ indexMig.js
@@ -5,43 +5,16 @@ const newTask = document.getElementById("new_task");
 
 function addTask(){
     
-    const newElements=[];
+   
     const submittedValue = newTask.value;
     if(!submittedValue)
     {
         alert("Please add new task first.")
         return;
     }
-    const newItemDiv = document.createElement("div")
-    newItemDiv.classList.add("form-check");
-    const newCheckInput = document.createElement("input");
-    newCheckInput.classList.add("form-check-input");
-    newCheckInput.type="checkbox";
-    newElements.push(newCheckInput);
     
-    const newItemLabel = document.createElement("label");
-    newItemLabel.classList.add("form-check-label");
-    newItemLabel.innerText=submittedValue;
-    newElements.push(newItemLabel);
-    const editButton = document.createElement("button");
-    editButton.innerText="Edit";
-    editButton.classList.add("btn");
-    editButton.classList.add("btn-success");
-    editButton.addEventListener("click",editTask);
-    newElements.push(editButton);
-    const removeButton = document.createElement("button");
-    removeButton.innerText="Remove";
-    removeButton.classList.add("btn");
-    removeButton.classList.add("btn-danger");
-    removeButton.addEventListener("click",removeTask);
-    newElements.push(removeButton);
-   for(let i = 0; i<newElements.length;i++)
-   {
-        newItemDiv.appendChild(newElements[i]);
-   }
-   
     
-    taskList.appendChild(newItemDiv)
+    taskList.appendChild(createListElements(submittedValue))
     newTask.value="";
 }
 
@@ -54,4 +27,49 @@ function editTask(){
 function removeTask(){
     taskList.removeChild(this.parentNode);
    
+}
+
+function createListElements(value)
+{
+    const newElements=[];
+    //creates div parent element and add its classes
+    const newItemDiv = document.createElement("div")
+    newItemDiv.classList.add("form-check");
+
+    //creates check input element,adds its classes and pushes it to newElements array
+    const newCheckInput = document.createElement("input");
+    newCheckInput.classList.add("form-check-input");
+    newCheckInput.type="checkbox";
+    newElements.push(newCheckInput);
+    
+    //create label element,adds its classes and values and pushes it to newElements array
+    const newItemLabel = document.createElement("label");
+    newItemLabel.classList.add("form-check-label");
+    newItemLabel.innerText=value;
+    newElements.push(newItemLabel);
+
+    //creates edit and remove buttons,add its classes and pushes them to newElements array
+    const editButton = document.createElement("button");
+    editButton.innerText="Edit";
+    editButton.classList.add("btn");
+    editButton.classList.add("btn-success");
+    editButton.addEventListener("click",editTask);
+    newElements.push(editButton);
+    const removeButton = document.createElement("button");
+    removeButton.innerText="Remove";
+    removeButton.classList.add("btn");
+    removeButton.classList.add("btn-danger");
+    removeButton.addEventListener("click",removeTask);
+    newElements.push(removeButton);
+
+    
+   for(let i = 0; i<newElements.length;i++)
+   {
+        newItemDiv.appendChild(newElements[i]);
+   }
+   
+
+
+
+    return newItemDiv;
 }
