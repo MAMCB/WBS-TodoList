c628325b45e6f41347f0edf6eba1c0e9ff0ee655
CiaranW12 <ciaran.walker12@gmail.com>
2023-11-08 16:11:26 +0100

    added navbar/fixed bootstrap compatibility issues with classes

--- scriptC.js
+++ scriptC.js
@@ -1,11 +1,12 @@
 const inputBox =document.getElementById("input-box")
-const listContainer = document.getElementById("list-container");
+const listContainer = document.getElementById("listOfToDo");
 
 function addTask() {
     if (inputBox.value === '') {
         alert("Please enter a task");
     } else {
         let li = document.createElement("li");
+        li.classList.add("toDoList")
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
         let span = document.createElement("span");
